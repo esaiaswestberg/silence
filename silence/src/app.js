c9a86a92564cb40e8c5e6ca2c309911d572bc7db
Esaias Westberg <esaias@westbergs.se>
2023-10-03 10:14:44 +0200

    Removed accidental compiled TS

--- silence/src/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-var silenceService_1 = require("./services/silenceService");
-silenceService_1.default.initialize();
