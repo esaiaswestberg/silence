68b53e750bb38d18719ae809dd250d67132db8d7
Esaias Westberg <esaias@westbergs.se>
2023-10-04 19:49:04 +0200

    Basic sponsor segment model

---
+++ ad-sens/model.py
@@ -0,0 +1,35 @@
+import os
+
+from keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, Flatten
+
+
+class Model:
+    model = Sequential()
+
+    def __init__(__self__, max_words, max_sequence_length):
+        __self__.model.add(Embedding(max_words, 32, input_length=max_sequence_length))
+        __self__.model.add(Flatten())
+        __self__.model.add(Dense(32, activation="relu"))
+        __self__.model.add(Dense(1, activation="sigmoid"))
+
+        if __self__.weight_file_exists():
+            __self__.load_weights()
+
+        __self__.model.compile(
+            loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"]
+        )
+
+    def fit(__self__, x, y, x_val, y_val, epochs=10, batch_size=32):
+        __self__.model.fit(
+            x, y, epochs=epochs, batch_size=batch_size, validation_data=(x_val, y_val)
+        )
+
+    def weight_file_exists(__self__):
+        return os.path.isfile("model/weights.h5")
+
+    def save_weights(__self__):
+        __self__.model.save_weights("model/weights.h5")
+
+    def load_weights(__self__):
+        __self__.model.load_weights("model/weights.h5")
---
+++ ad-sens/train.py
@@ -0,0 +1,72 @@
+import os
+
+import numpy as np
+import tensorflow as tf
+from model import Model
+from tensorflow.keras.layers import Dense, Embedding, Flatten
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+# Define the paths to your dataset
+positive_dir = "dataset/train/positive"
+negative_dir = "dataset/train/negative"
+
+
+# Load and preprocess the data
+def load_data(directory):
+    texts = []
+    labels = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            with open(os.path.join(directory, filename), "r", encoding="utf-8") as file:
+                text = file.read()
+                texts.append(text)
+                labels.append(
+                    1 if "positive" in directory else 0
+                )  # 1 for positive, 0 for negative
+    return texts, labels
+
+
+positive_texts, positive_labels = load_data(positive_dir)
+negative_texts, negative_labels = load_data(negative_dir)
+
+texts = positive_texts + negative_texts
+labels = positive_labels + negative_labels
+
+# Tokenize the text data and pad sequences
+max_sequence_length = 1000  # Adjust this based on your dataset
+max_words = 10000  # Maximum number of unique words in the vocabulary
+
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+data = pad_sequences(sequences, maxlen=max_sequence_length)
+
+labels = np.array(labels)
+
+# Split the data into training and validation sets
+validation_split = 0.2
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+num_validation_samples = int(validation_split * data.shape[0])
+
+x_train = data[:-num_validation_samples]
+y_train = labels[:-num_validation_samples]
+x_val = data[-num_validation_samples:]
+y_val = labels[-num_validation_samples:]
+
+# Train the model
+model = Model(max_words, max_sequence_length)
+model.fit(x_train, y_train, x_val, y_val)
+model.save_weights()
+
+# Evaluate the model
+loss, accuracy = model.model.evaluate(x_val, y_val)
+print(f"Validation loss: {loss:.4f}, Validation accuracy: {accuracy:.4f}")
+
+# Save the tokenizer
+with open("model/tokenizer.json", "w", encoding="utf-8") as file:
+    file.write(tokenizer.to_json())
